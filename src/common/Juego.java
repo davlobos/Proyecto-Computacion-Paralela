2ba8ab619963693e9441ae9f40af13eaefd4aada
sebastian019 <127500554+sebastian019@users.noreply.github.com>
2025-05-11 20:52:11 -0400

    a

---
+++ src/common/Juego.java
@@ -0,0 +1,29 @@
+package common;
+
+import java.io.Serializable;
+
+public class Juego implements Serializable {
+    private String nombre;
+    private int id;
+
+    public Juego(String nombre, int id) {
+    	this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
